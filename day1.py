d0a3512bb85742307baf29ee10e24c789d328a39
Brendan Swigart <32026702+Bren-Square@users.noreply.github.com>
2024-12-06 16:23:31 -0500

    Create day1.py

---
+++ day1.py
@@ -0,0 +1,37 @@
+"""
+    AoC Day 1
+    Brendan Swigart
+    This script assumes that you've pre-parsed your input
+    I removed whitespace using vscode regex, replacing it
+    with commas and then removed newline characters to make
+    one really long string to read into this program.
+"""
+
+# get values
+PATH = 'day1.txt'
+with open(PATH, 'r', encoding="utf-8") as file:
+    numbers = file.read()
+
+# setup lists.
+number_list = numbers.split(',')
+left_numbers = number_list[0::2]
+left_numbers = sorted(left_numbers, key=int)
+right_numbers = number_list[1::2]
+right_numbers = sorted(right_numbers, key=int)
+total_distance = 0 # pylint: disable=invalid-name
+
+# iterate and add
+for i, _ in enumerate(left_numbers):
+    delta = int(left_numbers[i]) - int(right_numbers[i])
+    total_distance += abs(int(delta))
+# print answer
+print(total_distance)
+
+# Part 2
+similarity_sum = 0
+for i, _ in enumerate(left_numbers):
+    number_count = right_numbers.count(left_numbers[i])
+    similarity_score = int(number_count) * int(left_numbers[i])
+    similarity_sum += similarity_score
+
+print(similarity_sum)
